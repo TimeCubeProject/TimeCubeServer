8dff34356cbf0929dd6587062f6ed55e14a61075
Drago <dragolegend.pl@gmail.com>
2024-06-01 16:50:19 +0200

    added logs and online logs console

--- index.js
+++ index.js
@@ -17,20 +17,46 @@ if (!process.env.GOOGLE_API_ID || !process.env.GOOGLE_API_SECRET || !process.env
     throw new Error("One or more keys missing, check .env file for missing keys");
 }
 
+exp.set('view engine', 'ejs');
+
+
+let logs = [];
+let max_log_length = 100;
+
+function add_log (log) {
+    if (!log instanceof String) {
+        return false;
+    }
+
+    logs.push(log);
+
+    if (logs.length > max_log_length) {
+        logs.shift();
+    }
+
+    return logs;
+}
+
+exp.get('/', (req, res) => {
+    res.render('page', { logs: logs });
+});
+
+
+
 const GOOGLE_CLIENT_ID = `${process.env.GOOGLE_API_ID}`;
 const GOOGLE_CLIENT_SECRET = `${process.env.GOOGLE_API_SECRET}`;
 
 exp.use(express.json());
 exp.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'OPTIONS'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Authorization'], // Allow these headers
-  optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'OPTIONS'], // Allow these methods
+    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Authorization'], // Allow these headers
+    optionsSuccessStatus: 204 // Some legacy browsers choke on 204
 }));
 // const corsOptions = {
-  // origin: 'https://timecubeproject.github.io/',//(https://your-client-app.com)
-  // optionsSuccessStatus: 200,
- //};
+// origin: 'https://timecubeproject.github.io/',//(https://your-client-app.com)
+// optionsSuccessStatus: 200,
+//};
 
 //exp.use(cors(corsOptions));
 
@@ -119,21 +145,20 @@ exp.get('/auth/google/callback',
     });
 
 exp.post("/update", (req, res) => {
+    add_log(`Endpoint: /update - Request: ${JSON.stringify(req.body)}`);
     app.update_cube(req.body.mac, req.body.id, req.body.currentWall, res).then((e) => {
-        DB.get_user_projects(1).then((result) => {
-           	console.log(req.body)
-	        res.send(result)
-        });
+            console.log(req.body);
+            res.send(e);
     });
 });
 
 //exp.get("/update", (req, res) => {
-    //app.update_cube(req.query.mac, req.query.id, req.query.currentWall, res).then((e) => {
-       // DB.get_user_projects(1).then((result) => {
-        //        console.log(req.query)
-      //          res.send(result)
-    //    });
-  //  });
+//app.update_cube(req.query.mac, req.query.id, req.query.currentWall, res).then((e) => {
+// DB.get_user_projects(1).then((result) => {
+//        console.log(req.query)
+//          res.send(result)
+//    });
+//  });
 //});
 
 function req_validator(token, fields = []) {
@@ -162,11 +187,13 @@ function req_validator(token, fields = []) {
     }
 }
 
-exp.post('/get_user_projects', (req, res) => { //podajesz token dostajesz tablice projektów
-    let validator_result = req_validator(req.body.token);
+exp.post('/get_user_projects', (req, res) => {
+    add_log(`Endpoint: /get_user_projects - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /get_user_projects - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
@@ -185,15 +212,18 @@ exp.post('/get_user_projects', (req, res) => { //podajesz token dostajesz tablic
                 e.CubeID = null;
             });
             res.send(result);
+            add_log(`Endpoint: /get_user_projects - Response: ${JSON.stringify(result)}`);
         });
     });
 });
 
-exp.post('/get_user_cubes', (req, res) => { //podajesz token dostajesz tablice kostek
-    let validator_result = req_validator(req.body.token);
+exp.post('/get_user_cubes', (req, res) => {
+    add_log(`Endpoint: /get_user_cubes - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /get_user_cubes - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
@@ -203,109 +233,145 @@ exp.post('/get_user_cubes', (req, res) => { //podajesz token dostajesz tablice k
             e.CubeID = null;
         });
         res.send(result);
+        add_log(`Endpoint: /get_user_cubes - Response: ${JSON.stringify(result)}`);
     });
 });
 
-exp.post('/get_events', (req, res) => { //podajesz token i id projektu dostajesz tablice eventów (start czas, stop czasu) dla projektu
-    let validator_result = req_validator(req.body.token, [req.body.project_id]);
+exp.post('/get_events', (req, res) => {
+    add_log(`Endpoint: /get_events - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token, [req.body.project_id]);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /get_events - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
     DB.get_events(app.get_id_from_token(req.body.token), req.body.project_id).then((result) => {
         res.send(result);
+        add_log(`Endpoint: /get_events - Response: ${JSON.stringify(result)}`);
     });
 });
 
-exp.post('/add_project', (req, res) => { // podajesz token, nazwę - dodaje projekt o tej nazwie
-    let validator_result = req_validator(req.body.token, [req.body.name]);
+exp.post('/add_project', (req, res) => {
+    add_log(`Endpoint: /add_project - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token, [req.body.name]);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /add_project - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
     DB.add_project(app.get_id_from_token(req.body.token), req.body.name).then((result) => {
         res.send(result);
+        add_log(`Endpoint: /add_project - Response: ${JSON.stringify(result)}`);
     });
 });
 
-exp.post('/remove_project', (req, res) => { // podajesz token, id projektu - usuwa projekt o danym id
-    let validator_result = req_validator(req.body.token, [req.body.project_id]);
+exp.post('/remove_project', (req, res) => {
+    add_log(`Endpoint: /remove_project - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token, [req.body.project_id]);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /remove_project - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
     DB.remove_project(app.get_id_from_token(req.body.token), req.body.project_id).then((result) => {
         res.send(result);
+        add_log(`Endpoint: /remove_project - Response: ${JSON.stringify(result)}`);
     });
 });
 
-exp.post('/set_project_active', (req, res) => { // podajesz token, id projektu, id kostki, mac kostki i ściankę - ustawiasz projekt na danej ściance danej kostki, jeśli mac kostki i id kostki = null to usuwasz kostkę z projektu
-    let validator_result = req_validator(req.body.token, [req.body.project_id]);
+exp.post('/set_project_active', (req, res) => {
+    add_log(`Endpoint: /set_project_active - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token, [req.body.project_id]);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /set_project_active - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
     if (req.body.cube_mac == null && req.body.cube_id == null) {
         DB.set_project_active(app.get_id_from_token(req.body.token), req.body.project_id, null, -1).then((result) => {
             res.send(result);
+            add_log(`Endpoint: /set_project_active - Response: ${JSON.stringify(result)}`);
         });
     } else if (req.body.cube_mac == null || req.body.cube_id == null) {
-        res.send({
+        const response = {
             success: false,
             code: 103,
             error: "Invalid request"
-        });
+        };
+        res.send(response);
+        add_log(`Endpoint: /set_project_active - Response: ${JSON.stringify(response)}`);
     } else if (req.body.side) {
         DB.get_cube(req.body.cube_mac, req.body.cube_id).then((e) => {
             if (e) {
                 DB.set_project_active(app.get_id_from_token(req.body.token), req.body.project_id, e.CubeID, req.body.side).then((result) => {
                     res.send(result);
+                    add_log(`Endpoint: /set_project_active - Response: ${JSON.stringify(result)}`);
                 });
             } else {
-                res.send(false);
+                const response = false;
+                res.send(response);
+                add_log(`Endpoint: /set_project_active - Response: ${JSON.stringify(response)}`);
             }
         });
     } else {
-        res.send({
+        const response = {
             success: false,
             code: 102,
             error: "Missing request fields"
-        });
+        };
+        res.send(response);
+        add_log(`Endpoint: /set_project_active - Response: ${JSON.stringify(response)}`);
     }
-
-
 });
 
-exp.post('/add_cube', (req, res) => { // podajesz token, mac kostki i id kostki - dodajesz kostkę
-    let validator_result = req_validator(req.body.token, [req.body.cube_mac, req.body.cube_id]);
+exp.post('/add_cube', (req, res) => {
+    add_log(`Endpoint: /add_cube - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token, [req.body.cube_mac, req.body.cube_id]);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /add_cube - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
     DB.add_cube(app.get_id_from_token(req.body.token), req.body.cube_mac, req.body.cube_id).then((result) => {
         res.send(result);
+        add_log(`Endpoint: /add_cube - Response: ${JSON.stringify(result)}`);
     });
 });
 
-exp.post('/remove_cube', (req, res) => { // podajesz token, mac kostki i id kostki - usuwasz kostkę
-    let validator_result = req_validator(req.body.token, [req.body.cube_mac, req.body.cube_id]);
+exp.post('/remove_cube', (req, res) => {
+    add_log(`Endpoint: /remove_cube - Request: ${JSON.stringify(req.body)}`);
 
+    let validator_result = req_validator(req.body.token, [req.body.cube_mac, req.body.cube_id]);
     if (!validator_result.success) {
         res.send(validator_result);
+        add_log(`Endpoint: /remove_cube - Response: ${JSON.stringify(validator_result)}`);
         return false;
     }
 
     DB.remove_cube(app.get_id_from_token(req.body.token), req.body.cube_mac, req.body.cube_id).then((result) => {
         res.send(result);
+        add_log(`Endpoint: /remove_cube - Response: ${JSON.stringify(result)}`);
+    });
+});
+
+
+
+
+
+
+
+exp.get('/logs', (req, res) => {
+    res.render('index', {
+        logs: logs
     });
 });
